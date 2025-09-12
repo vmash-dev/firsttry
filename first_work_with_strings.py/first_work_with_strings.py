f99d69159e4a2586bdefec837068a248b07b13cf
Your Name <you@example.com>
2025-09-13 02:41:56 +0300

    setup

---
+++ first_work_with_strings.py/first_work_with_strings.py
@@ -0,0 +1,21 @@
+smile = "😉"
+name1 = "Ілля"
+name2 = "Андрій"
+name3 = "   Софія"
+name4 = "максим"
+name5 = "Маша"
+cool_friends = "- ви класні друзяки"
+Sus = ("=")
+
+header = Sus * 30
+
+name3 = name3.strip()
+name4 = name4.title()
+
+friends = f"{name1}{smile} {name2}{smile} {name3}{smile} {name4}{smile} {name5}{smile} {cool_friends} "
+
+print(header)
+
+print(friends)
+
+print(header)
