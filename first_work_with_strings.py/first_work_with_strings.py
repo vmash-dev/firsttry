520742a7148f28aa6e9a95449ca9210c7857e325
Your Name <you@example.com>
2025-09-13 12:24:05 +0300

    cr

--- first_work_with_strings.py/first_work_with_strings.py
+++ first_work_with_strings.py/first_work_with_strings.py
@@ -1,23 +1,17 @@
 smile = "😉"
-name1 = "Ілля"
-name2 = "Андрій"
-name3 = "   Софія"
-name4 = "максим"
-name5 = "Маша"
+user_input = input("ввести 5 імен однокласників, через пробіл: ")
+user_input = user_input.strip()
+user_input = user_input.title()
+user_input = user_input.replace(" ", smile)
 cool_friends = "- ви класні друзяки"
-Sus = ("=")
+sus = ("=")
 
-header = Sus * 30
+header = sus * 30
 
-name3 = name3.strip()
-name4 = name4.title()
-
-friends = f"{name1}{smile} {name2}{smile} {name3}{smile} {name4}{smile} {name5}{smile} {cool_friends} "
+friends = f"{user_input} {cool_friends} "
 
 print(header)
 
 print(friends)
 
 print(header)
-
-
